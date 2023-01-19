fd06293ae88e77a9acd3e63686acb1e5a1ccbb52
marcoslenineog <marcoslenineog@gmail.com>
2023-01-19 10:26:33 -0300

    Update lpfunctions.js

--- lpfunctions.js
+++ lpfunctions.js
@@ -1,51 +1,58 @@
-function showModal() {
-  document.getElementById("modal").style.display = "block";
-  generateJoke();
-}
+// Declare sua API Key como uma variável global
+const apiKey = 'SUA_API_KEY_AQUI';
+
 // Função para chamar a API do OpenAI e gerar o trocadilho
-function generateJoke() {
-    // Obtendo o tema inserido pelo usuário
-    var theme = document.getElementById("input_frase").value;
-  
-    // Configurando a chamada para a API do OpenAI com o tema inserido pelo usuário
-    var settings = {
-      "url": "https://api.openai.com/v1/engines/davinci-codex/completions",
-      "method": "POST",
-      "timeout": 0,
-      "headers": {
-        "Content-Type": "application/json",
-        "Authorization": "Bearer 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"
+async function generateJoke(topic) {
+  try {
+    // Utilizando o Fetch API para fazer a chamada à API do OpenAI
+    const response = await fetch(`https://api.openai.com/v1/engines/davinci/completions`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${apiKey}`
       },
-      "data": JSON.stringify({
-        "prompt": "Crie um trocadilho infame sobre " + theme + " e que faça sentido como uma história, porém cômica",
-        "max_tokens": 600
-      }),
-    };
-  
-    // Fazendo a chamada para a API e obtendo a resposta
-    $.ajax(settings).done(function (response) {
-      // Atualizando o texto dentro do modal com o trocadilho gerado
-      document.getElementById("trocadilho").innerHTML = response.choices[0].text;
+      body: JSON.stringify({
+        prompt: `Crie um trocadilho infame sobre ${topic} e que faça sentido como uma história, porém cômica.`,
+        max_tokens: 600
+      })
     });
+    // Convertendo a resposta para JSON
+    const data = await response.json();
+    // Recuperando o trocadilho gerado
+    const joke = data.choices[0].text;
+    // Atualizando o texto dentro do modal com o trocadilho gerado
+    document.getElementById("trocadilho").innerHTML = joke;
+  } catch (err) {
+    console.error(err);
   }
-  
-  // Abrir o modal quando o botão "Gerar trocadilho" é clicado
-  function openModal() {
+}
+
+// Função para abrir o modal
+function showModal() {
+  // Recuperando o valor do campo de texto com o tópico
+  const topic = document.getElementById("topic").value;
+  // Validando se o campo de texto foi preenchido
+  if (topic) {
+    // Abrindo o modal
     document.getElementById("modal").style.display = "block";
+    // Gerando o trocadilho
+    generateJoke(topic);
   }
-  
-  // Fechar o modal quando o botão "Fechar" é clicado
-  function closeModal() {
-    document.getElementById("modal").style.display = "none";
-  }
-  
-  // Adicionando eventos de clique aos botões "Gerar trocadilho" e "Fechar"
-  document.getElementById("gerar-trocadilho").addEventListener("click", function() {
-    if (document.getElementById("input_frase").value != "") {
-      generateJoke();
-      openModal();
-    }
-  });
-  
-  document.getElementById("fechar-modal").addEventListener("click", closeModal);
-  
+}
+
+// Função para fechar o modal
+function closeModal() {
+  document.getElementById("modal").style.display = "none";
+}
+
+// Adicionando eventos de clique aos botões "Gerar trotrocadilho" e "Fechar"
+document.getElementById("gerar-trocadilho").addEventListener("click", showModal);
+document.getElementById("close-modal").addEventListener("click", closeModal);
+
+// Adicionando evento de tecla "Enter" para o campo de texto com o tópico
+document.getElementById("topic").addEventListener("keyup", function(event) {
+event.preventDefault();
+if (event.keyCode === 13) {
+document.getElementById("gerar-trocadilho").click();
+}
+});
